7eb63e93cc2cf276c179f6d5774daba220760cd1
sofia Luna <Sofia21luna@gmail.com>
2020-09-09 13:42:21 -0600

    navbar is completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import "./App.css";
+import CardNavbar from './components/navbar/navbar'
 
 function App() {
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+     <CardNavbar />
     </div>
   );
 }
---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./navbar.css";
+
+const CardNavbar = () => {
+
+    return(
+        <div>
+            <navbar className="navbar">
+                <h1>Nasa's Photo of The Day</h1>
+            </navbar>
+        </div>
+    )
+}
+
+export default CardNavbar;
