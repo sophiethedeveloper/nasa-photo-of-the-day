bdd30deb1d9b8c2d45480011f79e4764f82ac854
sofia Luna <Sofia21luna@gmail.com>
2020-09-10 14:06:39 -0600

    added styled components and add styled card and navbar

--- src/components/card/NasaCard.js
+++ src/components/card/NasaCard.js
@@ -1,12 +1,53 @@
 import React from "react";
 import "./card.css";
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+    font-family: 'Staatliches', cursive;
+    background-color: rgba(220, 220, 220, 0.2);
+    color: black;
+    width: 60%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 20px;
+    cursor: pointer;
+    box-shadow: 0px 1px 6px -2px grey;
+
+    .nasa-image {
+    width: 45%;
+   
+  }
+
+  .date,
+  .credit {
+    display: flex;
+    font-weight: bold;
+    padding: 15px;
+    margin: 20px;
+  }
+
+  .title{
+    font-size: 20px;
+    font-weight: bold;
+    color: #ffff;
+  }
+  
+  .description {
+    display: flex;
+    justify-content: flex-start;
+    font-size: 14px;
+    margin-top: 20px;
+  }
+`;
 
 const NasaCard = (props) => {
 
     const { title, photo, date, description, credit } = props
   return (
     <div className="container">
-      <div className="nasa-card">
+      <StyledDiv>
         <h2 className="title">{title}</h2>
         <img
           className="nasa-image"
@@ -16,7 +57,7 @@ const NasaCard = (props) => {
         <p className='description'>{`Description: ${description}`}</p>
         <p className="date">{date}</p>
         <p className="credit">{credit}</p>
-      </div>
+      </StyledDiv>
     </div>
   );
 };
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import "./navbar.css";
+import React from "react";
+import styled from "styled-components";
+
+const StyledNav = styled.div`
+  color: #ffffff;
+  background-color: rgba(220, 220, 220, 0.2);
+  font-family: "Nanum Myeongjo", serif;
+  font-size: 1.2rem;
+  height: 16vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 
 const CardNavbar = () => {
-
-    return(
-        <div>
-            <nav className="navbar">
-                <h1>Nasa's Photo of The Day</h1>
-            </nav>
-        </div>
-    )
-}
+  return (
+    <StyledNav>
+      <nav>
+        <h1>Nasa's Photo of The Day</h1>
+      </nav>
+    </StyledNav>
+  );
+};
 
 export default CardNavbar;
